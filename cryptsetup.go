52c5cd5ae4953bda5001535978bdfba5a05a8a9f
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2016-08-07 18:50:01 -0300

    Added basic crypt_init() & crypt_format() support.

--- cryptsetup.go
+++ cryptsetup.go
@@ -1,10 +1,11 @@
-package main
+package cryptsetup
 
 /*
-#cgo CFLAGS: -O3
+#cgo CFLAGS: -O2
 #cgo LDFLAGS: -lcryptsetup
 #include <libcryptsetup.h>
 #include <stdlib.h>
+#include <stdio.h>
 */
 import "C"
 
@@ -13,68 +14,81 @@ import (
 	"unsafe"
 )
 
-type Node struct {
-	Name string
-	Path string
-	Passphrase string
+type Error struct {
+	code int
+	function string
+}
+
+type LUKSParams struct {
+	data_alignment int
+	data_device, hash string
+}
+
+type LoopAESParams struct {
+	hash string
+	offset, skip int
+}
+
+type CryptDevice struct {
 	device *C.struct_crypt_device
 }
 
-type Error struct {
-	message string
-	code int
-}
-
 func (e *Error) Error() string {
-	return fmt.Sprintf("%s: %d", e.message, e.code)
+	return fmt.Sprintf("%s returned error with code %d", e.function, e.code)
 }
 
-func Load_Node(name string, path string, passphrase string, nodes *[]Node) (error) {
-	var device *C.struct_crypt_device
-	c_path := C.CString(path)
-	c_crypt_luks := C.CString(C.CRYPT_LUKS1)
-	c_name := C.CString(name)
-	c_passphrase := C.CString(passphrase)
-	defer C.free(unsafe.Pointer(c_path))
-	defer C.free(unsafe.Pointer(c_crypt_luks))
-	defer C.free(unsafe.Pointer(c_name))
-	defer C.free(unsafe.Pointer(c_passphrase))
+func Init(device string) (error, *CryptDevice) {
+	c_device := C.CString(device)
+	defer C.free(unsafe.Pointer(c_device))
 
-	err := C.crypt_init(&device, c_path)
+	var c_cd *C.struct_crypt_device
+
+	err := C.crypt_init(&c_cd, c_device)
 	if err < 0 {
-		return &Error{message: "crypt_init", code: int(err)}
-	}
-	defer C.crypt_free(device)
-
-	err = C.crypt_load(device, c_crypt_luks, nil)
-	if err < 0 {
-		return &Error{message: "crypt_load", code: int(err)}
+		return &Error{function: "crypt_init", code: int(err)}, nil
 	}
 
-	err = C.crypt_activate_by_passphrase(device, c_name, C.CRYPT_ANY_SLOT,
-		c_passphrase, C.size_t(len(passphrase)), 0)
-	if err < 0 && err != -17 {
-		return &Error{message: "crypt_activate_by_passphrase", code: int(err)}
-	}
-
-	node := Node{Name: name, Path: path, Passphrase: passphrase, device: device}
-	*nodes = append(*nodes, node)
-
-	return nil
+	return nil, &CryptDevice{device: c_cd}
 }
 
-func Unload_Nodes(nodes []Node) (error) {
-	for _, node := range nodes {
-		name := C.CString(node.Name)
-		defer C.free(unsafe.Pointer(name))
+func (device *CryptDevice) FormatLUKS(cipher string, cipher_mode string, uuid string, volume_key string, volume_key_size int, params LUKSParams) (error) {
+	cstr_type := C.CString(C.CRYPT_LUKS1)
+	defer C.free(unsafe.Pointer(cstr_type))
 
-		err := C.crypt_deactivate(node.device, name)
-		if err < 0 {
-			return &Error{message: "crypt_deactivate", code: int(err)}
-		}
+	cstr_cipher := C.CString(cipher)
+	defer C.free(unsafe.Pointer(cstr_cipher))
 
-		// crypt_free() is raising a SIGPANIC, so it is commented for now
-		// C.crypt_free(node.device)
+	cstr_cipher_mode := C.CString(cipher_mode)
+	defer C.free(unsafe.Pointer(cstr_cipher_mode))
+
+	var cstr_uuid *C.char
+	if (uuid == "") {
+	    cstr_uuid = nil
+	} else {
+	    cstr_uuid = C.CString(uuid)
+	    defer C.free(unsafe.Pointer(cstr_uuid))
+	}
+
+	var cstr_volume_key *C.char
+	if (volume_key == "") {
+	    cstr_volume_key = nil
+	} else {
+	    cstr_volume_key = C.CString(volume_key)
+	    defer C.free(unsafe.Pointer(cstr_volume_key))
+	}
+
+	var c_params C.struct_crypt_params_luks1
+	c_params.data_alignment = C.size_t(params.data_alignment)
+	c_params.hash = C.CString(params.hash)
+	if params.data_device != "" {
+	    c_params.data_device = C.CString(params.data_device)
+	} else {
+	    c_params.data_device = nil
+	}
+
+	err := C.crypt_format(device.device, cstr_type, cstr_cipher, cstr_cipher_mode, cstr_uuid, cstr_volume_key, C.size_t(volume_key_size), unsafe.Pointer(&c_params))
+	if err < 0 {
+		return &Error{function: "crypt_format", code: int(err)}
 	}
 
 	return nil
